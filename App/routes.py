ba1115371aa076e204940deffeb551bea732b17f
PB <pab15@humboldt.edu>
2020-01-27 18:49:57 -0800

    Started Blog

---
+++ App/routes.py
@@ -0,0 +1,14 @@
+from App import app
+from flask import redirect, url_for, render_template, request
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
---
+++ startvenv.ps1
@@ -0,0 +1,8 @@
+# Script that will initialize a virtual environment and install packages
+#        to use the flask application:
+
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+pip install flask
+pip install markdown
+
