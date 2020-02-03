97592a6a7e5bc44e7e363e8029198856b7f9ead3
PB <pab15@humboldt.edu>
2020-02-03 14:50:45 -0800

    Fixed styling errors, bad redirection, made all pg

--- App/routes.py
+++ App/routes.py
@@ -1,50 +1,95 @@
+import os
+import sqlite3
 from App import app
 from flask import redirect, url_for, render_template, request, session
-from flask_mail import Mail, Message
 
+# Database Initializer:
+db_connection = sqlite3.connect('database/flaskblog.db')
+
+curs = db_connection.cursor()
+
+# Use the cursor.execute() to use sql commands:
+# curs.execute("""CREATE TABLE users (
+#                 id integer,
+#                 username text,
+#                 email text,
+#                 password text
+#             )""")
+
+db_connection.commit()
+
+def addUser(username, email, password):
+    curs.execute("""INSERT INTO users (username, email, password)
+                    VALUES ({username}, {email}, {password})
+                """)
+    db_connection.commit()
+
+# Home Page if logged in, otherwise user if directed to login page:
 @app.route('/')
 def index():
     if "username" in session:
         return render_template('index.html')
     else:
         return render_template('login.html')
-
-@app.route('/all', methods=['GET', 'POST'])
+    
+# Shows a list of all pages in the website if admin is logged in,
+#   otherwise the user is redirected to the login page:
+@app.route('/all')
 def all():
+    view_data = {}
+    files = os.listdir(r"C:\Users\peter\Desktop\Courses\CS-232\Projects\flask-blog\App\templates")
+    view_data["pages"] = files
     if "username" in session:
-        return render_template('all.html')
+        return render_template('all.html', allfiles=view_data)
     else:
         return render_template('login.html')
 
+# Basic login page that makes sure the username and password fields
+#   are not blank and then creates a session for the user and redirects 
+#   them to the home page
+#   ToDo: VALIDATE THAT THE USER EXISTS IN THE DATABASE
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if (username == "") or (password == ""):
-             return render_template('login.html')
+            return render_template('login.html')
         else:
             session["username"] = username
             session["password"] = password
-            return render_template('index.html')
+            return redirect(url_for('index'))
     else:
-        return render_template('login.html')
+        if "username" in session:
+            return render_template('index.html')
+        else:
+            return render_template('login.html')
 
-@app.route('/signup')
+# Basic sign up page that checks to make sure that all fields are blank 
+#   and then redirects to the login page (does nothing) 
+#   information posted to the server
+# ToDo: ADD THE SIGNED UP USER TO THE DATABASE:
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         user_name = request.form['user_name']
         e_mail = request.form['e_mail']
         pass_word = request.form['pass_word']
         password_confirm = request.form['pass_word_validate']
-        if (user_name == "") or (e_mail == "") or (pass_word == "") or (password_confirm == ""):
+        if (user_name == "") or (e_mail == "") or (pass_word == "") or (password_confirm == "") or (pass_word != password_confirm):
              return render_template('sign-up.html')
         else:
+            addUser(user_name, e_mail, pass_word)
             return render_template('login.html')
     else:
         return render_template('sign-up.html')
 
-@app.route('/about')
+# About page that checks if the user is logged in and has a session, and if they
+#   do, returns a customized page for the user
+# ToDo: ALLOW THE USER TO CHANGE THEIR ABOUT/PROFILE AND PULL INFO FROM
+#       THE DATABASE TO DISPLAY TO THE USER, IF THEY CHANGE THIS INFO,
+#       UPDATE THE DATABASE:
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     if "username" in session:
         user = session['username']
@@ -52,6 +97,11 @@ def about():
     else:
         return render_template('login.html')
 
+# Contact form that posts the data to the server and returns a customized
+#   paged confirming that their message was sent, also will redirect the 
+#   user if they dont have a session
+#   ToDo: PUT THE MESSAGE INTO A DATABASE OR USE AN SMTP SERVER TO EMAIL 
+#         THE MESSAGE TO MYSELF
 @app.route('/contact/', methods=['GET', 'POST'])
 def contact():
     if "username" in session:
@@ -68,3 +118,10 @@ def contact():
             return render_template('contact.html')
     else:
         return render_template('login.html')
+
+# @app.route("/<view_name>")
+# #input parameter name must match route parameter
+# def render_page(view_name):
+#     html = 'view_name' + '.html'
+#     view_data = {} #create empty dictionary
+#     return render_template(html, view_data=session)
