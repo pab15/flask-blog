015b27a28f5a1419388cb25265ae687a7e826f0c
PB <pab15@humboldt.edu>
2020-02-11 18:39:36 -0800

    GIF and Design Diary

--- App/__init__.py
+++ App/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.secret_key = b'thisisabadsessionprivatekey12354643'
+app.secret_key = b'thisisabadsessionprivatekey1235464345'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 from App import routes
